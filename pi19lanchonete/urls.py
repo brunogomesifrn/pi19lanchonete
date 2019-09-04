aef384651fc29e730cb98f4a3e1d607b13476d9b
Natália Cristina Tertuliano Melo <natalia.cristina@academico.ifrn.edu.br>
2019-09-04 11:34:21 -0300

    pagCadPrato

--- pi19lanchonete/urls.py
+++ pi19lanchonete/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from core.views import index, menu, pedido, cadastro, login, perfil, dados
 
 urlpatterns = [
